77fed8781fd9db49bbc8585afe2dc68676b4d94a
Jonathan Diep <jonathandiep@hotmail.com>
2016-02-18 13:33:02 -0800

    ES6 string literals

--- index.js
+++ index.js
@@ -26,7 +26,7 @@ app.use(((req, res, next) => {
 app.use(morgan('dev'));
 
 app.use(express.static(__dirname + '/public'));
-app.use('/bower_components', express.static(__dirname + '/bower_components'));
+app.use('/bower_components', express.static(`${__dirname}/bower_components`));
 
 client.connect( (err) => {
   if (err) { return console.error('could not connect to postgres'); }
@@ -45,12 +45,12 @@ app.get('/product-in-categories', (req, res) => {
   var category = req.query.category;
   var type = req.query.type;
   if (type) {
-    client.query('SELECT * FROM "Product" INNER JOIN "Category" ON "Product"."categoryID" = "Category"."categoryID" WHERE "Category"."categoryName" = \'' + type + '\'', (err, result) => {
+    client.query(`SELECT * FROM "Product" INNER JOIN "Category" ON "Product"."categoryID" = "Category"."categoryID" WHERE "Category"."categoryName" = '${type}'`, (err, result) => {
       if (err) { return console.error('error running query'); }
       res.send(result.rows);
     });
   } else {
-    client.query('SELECT * FROM "Product" INNER JOIN "Category" ON "Product"."categoryID" = "Category"."categoryID" WHERE "Category"."parent" = \'' + category + '\'', (err, result) => {
+    client.query(`SELECT * FROM "Product" INNER JOIN "Category" ON "Product"."categoryID" = "Category"."categoryID" WHERE "Category"."parent" = '${category}'`, (err, result) => {
       if (err) { return console.error('error running query'); }
       res.send(result.rows);
     });
@@ -60,7 +60,7 @@ app.get('/product-in-categories', (req, res) => {
 // used for subcategories when viewing a category
 app.get('/category-list', (req, res) => {
   var parent = req.query.parent;
-  client.query('SELECT "categoryName" FROM "Category" WHERE "parent" = \'' + parent + '\'', (err, result) => {
+  client.query(`SELECT "categoryName" FROM "Category" WHERE "parent" = '${parent}'`, (err, result) => {
     if (err) { return console.error('error running query'); }
     res.send(result.rows);
   });
@@ -73,7 +73,7 @@ app.get('/search-products', (req, res) => {
     var append = "'" + term.substring(term.indexOf("'"));
     term = term.substring(0, term.indexOf("'")) + append;
   }
-  client.query('SELECT * FROM "Product" WHERE LOWER("productName") LIKE LOWER(\'%' + term + '%\') OR "productID" = \'' + term + '\'', (err, result) => {
+  client.query(`SELECT * FROM "Product" WHERE LOWER("productName") LIKE LOWER('%${term}%') OR "productID" = '${term}'`, (err, result) => {
     if (err) { return console.error('error running query'); }
     res.send(result.rows);
   });
@@ -86,7 +86,7 @@ app.get('/live-search', (req, res) => {
     var append = "'" + term.substring(term.indexOf("'"));
     term = term.substring(0, term.indexOf("'")) + append;
   }
-  client.query('SELECT * FROM "Product" WHERE LOWER("productName") LIKE LOWER(\'%' + term + '%\') OR "productID" = \'' + term + '\' LIMIT 3', (err, result) => {
+  client.query(`SELECT * FROM "Product" WHERE LOWER("productName") LIKE LOWER('%${term}%') OR "productID" = '${term}' LIMIT 4`, (err, result) => {
     if (err) { return console.error('error running query'); }
     res.send(result.rows);
   });
@@ -95,7 +95,7 @@ app.get('/live-search', (req, res) => {
 // used to display product detail
 app.get('/product-detail', (req, res) => {
   var id = req.query.id;
-  client.query('SELECT * FROM "Product" WHERE "productID" = \'' + id + '\'', (err, result) => {
+  client.query(`SELECT * FROM "Product" WHERE "productID" = '${id}'`, (err, result) => {
     if (err) { console.error('error running query'); }
     res.send(result.rows);
   });
@@ -104,15 +104,15 @@ app.get('/product-detail', (req, res) => {
 // used to get categories and subcategories when viewing a product
 app.get('/category-names', (req, res) => {
   var id = req.query.id;
-  client.query('SELECT * FROM "Category" WHERE "categoryID" = \'' + id + '\'', (err, result) => {
+  client.query(`SELECT * FROM "Category" WHERE "categoryID" = '${id}'`, (err, result) => {
     if (err) { console.error('error running query'); }
     res.send(result.rows);
   });
 });
 
 app.get('/*', (req, res) => {
-  res.sendFile(__dirname + '/public/index.html');
+  res.sendFile(`${__dirname}/public/index.html`);
 });
 
 app.listen(port);
-console.log('working on port: ' + port);
+console.log(`working on port: ${port}`);
